34594a2bd78da20b589ad121740398961428b0da
hung.bui <hung.bui@evotek.vn>
2020-04-12 13:43:34 +0700

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -3,7 +3,7 @@ console.log('js file');
 const getDataForecast = (address) => {
     messageOne.textContent = 'Loading...';
     messageTwo.textContent = '';
-    fetch(`http://localhost:3000/weather?address=${address}`).then((res) => {
+    fetch(`/weather?address=${address}`).then((res) => {
         res.json().then(data => {
             const { error, forecast, location } = data;
             if (error) {
